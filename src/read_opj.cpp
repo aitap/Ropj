e012020acb365a16ca08906542501818513aa871
aitap <krylov.r00t@gmail.com>
2019-01-29 01:07:54 +0300

    get spreadsheet import to work

--- src/read_opj.cpp
+++ src/read_opj.cpp
@@ -3,12 +3,39 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+#include "OriginFile.h"
+
 // [[Rcpp::export(name="read.opj")]]
 List read_opj(const std::string & file) {
+	OriginFile opj(file);
 
-    CharacterVector x = CharacterVector::create( "foo", "bar" )  ;
-    NumericVector y   = NumericVector::create( 0.0, 1.0 ) ;
-    List z            = List::create( x, y ) ;
+	if (!opj.parse()) stop("Failed to parse " + file); // throws
 
-    return z ;
+	List ret;
+
+	// only spreadsheets for now
+	for (unsigned int i = 0; i < opj.spreadCount(); i++) {
+		Origin::SpreadSheet & osp = opj.spread(i);
+
+		NumericMatrix rsp(osp.maxRows, osp.columns.size());
+		rsp.fill(NAN);
+
+		CharacterVector names(osp.columns.size());
+
+		for (unsigned int col = 0; col < osp.columns.size(); col++) {
+			names[col] = osp.columns[col].comment; // what's that with @ characters in there?
+			for (unsigned int row = 0; row < osp.columns[col].data.size(); row++) {
+				Origin::variant v(osp.columns[col].data[row]);
+				if (v.type() == Origin::variant::V_DOUBLE)
+					rsp(row,col) = v.as_double();
+			}
+		}
+
+		colnames(rsp) = names;
+		ret[osp.name] = rsp;
+	}
+
+	// TODO: dataset, spread, matrix, excel, function, graph, note
+
+    return ret;
 }
