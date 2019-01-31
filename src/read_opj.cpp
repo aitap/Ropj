71fe7f80f84571d21409f0b20efac3dd723f30a8
Ivan Krylov <krylov.r00t@gmail.com>
2019-01-31 21:11:23 +0300

    trying to implement unlist in R to produce vectors when reading mixed content; broken so far

--- src/read_opj.cpp
+++ src/read_opj.cpp
@@ -5,37 +5,63 @@ using namespace Rcpp;
 
 #include "OriginFile.h"
 
+template<int T, typename U> // REALSXP or STRSXP
+struct unlist {
+	static const U NA_value;
+	Vector<T> operator()(const List & list) {
+		// like R unlist, but supplement NULLs with NAs
+		Vector<T> ret(list.size());
+		for (unsigned int i = 0; i < list.size(); i++)
+			ret[i] = Rf_isNull(list[i]) ? NA_value : as<T>(list[i]);
+		return ret;
+	}
+};
+
+template<>
+const decltype(NA_STRING) unlist<STRSXP,decltype(NA_STRING)>::NA_value = NA_STRING;
+
+template<>
+const Vector<REALSXP> unlist<REALSXP,decltype(NA_REAL)>::NA_value = NA_REAL;
+
 // [[Rcpp::export(name="read.opj")]]
 List read_opj(const std::string & file) {
 	OriginFile opj(file);
 
-	if (!opj.parse()) stop("Failed to parse " + file); // throws
+	if (!opj.parse()) stop("Failed to open and/or parse " + file); // throws
 
 	List ret;
 
-	// only spreadsheets for now
 	for (unsigned int i = 0; i < opj.spreadCount(); i++) {
 		Origin::SpreadSheet & osp = opj.spread(i);
 
-		NumericMatrix rsp(osp.maxRows, osp.columns.size());
-		rsp.fill(NAN);
+		List rsp(osp.columns.size());
+		CharacterVector names(rsp.size()), comments(rsp.size()), commands(rsp.size());
 
-		CharacterVector names(osp.columns.size());
-
-		for (unsigned int col = 0; col < osp.columns.size(); col++) {
-			names[col] = osp.columns[col].comment; // what's that with @ characters in there?
-			for (unsigned int row = 0; row < osp.columns[col].data.size(); row++) {
-				Origin::variant v(osp.columns[col].data[row]);
+		for (unsigned int c = 0; c < osp.columns.size(); c++) {
+			names[c] = osp.columns[c].name;
+			comments[c] = osp.columns[c].comment; // user might want to split by \r\n...
+			commands[c] = osp.columns[c].command;
+			List col(osp.maxRows);
+			for (unsigned int row = 0; row < osp.columns[c].data.size(); row++) {
+				Origin::variant & v = osp.columns[c].data[row];
 				if (v.type() == Origin::variant::V_DOUBLE)
-					rsp(row,col) = v.as_double();
+					col[row] = v.as_double();
+				else
+					col[row] = v.as_string();
 			}
+			rsp[c] = Rcpp::Language("unlist", col).eval();
 		}
 
-		colnames(rsp) = names;
+		rsp.attr("names") = names;
+		rsp.attr("comments") = comments;
+		rsp.attr("commands") = commands;
 		ret[osp.name] = rsp;
 	}
 
-	// TODO: dataset, spread, matrix, excel, function, graph, note
+	// TODO: matrix, excel, graph, note
+
+	// FIXME: "dataset" and "function" are going to be untested
+	// because I cannot find them in my copy of Origin
 
     return ret;
 }
