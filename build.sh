a2539f5d27d4b80bc903e9e6806f9392ff93d16b
aitap <krylov.r00t@gmail.com>
2019-03-02 21:50:06 +0300

    build.sh: allow appending compiler flags like -fsanitize=...

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 R -q -e 'Rcpp::compileAttributes()'
 nproc=$(nproc) || nproc=1
-export MAKE="make -j$nproc" PKG_CXXFLAGS="-Wall -Wextra"
+export MAKE="make -j$nproc" PKG_CXXFLAGS="${PKG_CXXFLAGS} -Wall -Wextra"
 rm -vf Ropj_*.tar.gz
 R CMD build .
 # pass check [--as-cran] before releasing
