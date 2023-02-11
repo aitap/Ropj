cc94320bd5b8b68c879ff604787e71831fff8108
Ivan K <krylov.r00t@gmail.com>
2023-02-11 12:05:33 +0300

    configure: a better fix for the C++11 problem
    Since R-3.6.2, at least C++11 is guaranteed on supported R platforms
    (either that or no C++ at all), which suits us just fine. Use C++
    version detection on older R versions.
    
    CXX1X support appeared in R-3.1.0, so explicitly depend on that.

--- tools/configure.R
+++ tools/configure.R
@@ -1,16 +1,20 @@
 library(tools)
 
-for (std in c('CXX17', 'CXX14', 'CXX11', 'CXX1X')) {
-	cat('Checking if R knows a', std, 'compiler... ')
-	out <- suppressWarnings(Rcmd(paste('config', std)))
-	if (out == 0) break
+# Since R 3.6.2, C++ defaults to >=11 even on Windows
+cxx_std <- if (getRversion() >= '3.6.2') NULL else {
+	for (std in c('CXX17', 'CXX14', 'CXX11', 'CXX1X')) {
+		cat('Checking if R knows a', std, 'compiler... ')
+		out <- suppressWarnings(Rcmd(paste('config', std)))
+		if (out == 0) break
+	}
+	if (out != 0) stop("Couldn't find an appropriate compiler")
+	if (std == 'CXX1X') std <- 'CXX11'
+	paste('CXX_STD =', std)
 }
-if (out != 0) stop("Couldn't find a C++ >= 11 compiler")
-if (std == 'CXX1X') std <- 'CXX11'
 
 f <- file(file.path('src', 'Makevars'), 'wb')
 writeLines(c(
-	paste('CXX_STD =', std),
+	cxx_std,
 	'OBJECTS = liborigin.o RcppExports.o read_opj.o',
 	'PKG_CPPFLAGS = -I . -I liborigin'
 ), f)
